438d6b55c1fd5b4bf83179b7342e127c176e257a
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-08-30 23:20:43 +0000

    Update Special Arrays.js

--- Special Arrays.js
+++ Special Arrays.js
@@ -1,19 +1,21 @@
-let tableau=[2, 7, 4, 9, 6, 1, 6, 3]
+let tableau=[2, 1, 4, 9, 6, 1, 6, 3]
+let tableau2=[2, 2, 4, 9, 6, 1, 6, 3]
 
 function isSpecialArray(arr) {
 	for(let i=0; i<arr.length;i++){
         
         if(i%2==0){
-            if((arr[i])%2 == 0){
-                return true
+            if((arr[i])%2 !== 0){
+                return false
             }
         }
-        if(i%2 !==0){
-            if((arr[i])%2 !== 0){
-                return true
+        if(i%2!==0){
+            if((arr[i])%2 === 0){
+                return false
             }
         }
     }return true
 }
 
-isSpecialArray(tableau)
+console.log(isSpecialArray(tableau)) 
+console.log(isSpecialArray(tableau2))
