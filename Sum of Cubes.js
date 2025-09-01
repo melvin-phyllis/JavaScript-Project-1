d6d3def2f1744a5c28b096b22d8afd727b99d723
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-09-01 12:50:43 +0000

    Update Sum of Cubes.js

--- Sum of Cubes.js
+++ Sum of Cubes.js
@@ -1,13 +1,15 @@
 
 let tableau=[2, 7, 4, 9, 6, 1, 6, 3]
+let tableau2=[3,4,9,4,9]
 
 function sumOfCubes(nums){
     let sommes = 0
     for(let item of nums){
           sommes  += item**3     
     }
-    return sommes
+    return console.log(sommes)
     
 }
 
-console.log(sumOfCubes(tableau))
+sumOfCubes(tableau)
+sumOfCubes(tableau2)
