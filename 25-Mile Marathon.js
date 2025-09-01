2f3597fc9bdfc8361ca4ac8c50c4610011b48d5a
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-09-01 12:19:12 +0000

    Update 25-Mile Marathon.js

--- 25-Mile Marathon.js
+++ 25-Mile Marathon.js
@@ -1,16 +1,19 @@
-let tableau = [1, 9, 5, 8, 2];
+let array = [1, 9, 5, 8, 2];
+let array1 = [9, 5, 8,1, 9, 5, 8, 2];
 let somme = 0;
 
 function marathonDistance(d) {
+  //boucle qui permettra de parcourir le tableau tout en fesant la somme de c'est composant 
     for (let i = 0; i < d.length; i++) {
         somme += d[i];
     }
-
-    if (somme == 25) {
-        return true;
-    } else {
-        return false;
-    }
+//condition qui verifie si le total du tableau est = 25 ou nn 
+    if (somme === 25) {
+        return console.log(true);
+    } 
+        return console.log(false);
+    
 }
 
-console.log(marathonDistance(tableau));
+marathonDistance(array);
+marathonDistance(array1);
