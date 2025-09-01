14831f8838d9a97557ba5a9b97d8b44fcee58f31
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-09-01 12:29:26 +0000

    Update Filter out Strings from an Array.js

--- Filter out Strings from an Array.js
+++ Filter out Strings from an Array.js
@@ -1,15 +1,15 @@
 
+let tableau = [10,2, 35, 'n', 1, 34, 54];
 let tableau2 = ['n',, 35, 'n', 1, 34, 54];
 
-
+//fonction qui retire tout element du tableau qui n'est pas un nombre 
 function filterArray(arr) {
    
-           
+           //filtre les information et cree un nouveau tableau 
            const q = arr.filter(b => typeof b !== "string")
        return console.log(q) 
 }
 
 
-
+filterArray(tableau) 
 filterArray(tableau2) 
-
