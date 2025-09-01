ba7b22aac1e0c5934b1d2d6251f1ade4ab6f71d5
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-09-01 12:45:03 +0000

    Update Is the Number Symmetrical?.js

--- Is the Number Symmetrical?.js
+++ Is the Number Symmetrical?.js
@@ -1,21 +1,23 @@
 
-let a= 2922;
+let a= 45654;
 let b= 2222;
-
+let c= 198537;
+//fonction qui verifie la symetrie d'un nombre
 function isSymmetrical(num) {
     let n=0
+    //convertir le nombre en tableau 
     num = Array.from(num.toString(),Number)
+//boucle qui parrcoure le tableau tout en verifiant la symetrie du nombre 
 	for(let i=num.length-1;i>-1;i--){
             
             if(num[n]!==num[i]){
                 
-                    return false
-            
-            }else{
-                console.log(`${num[n]}=${num[i]}`)
+                    return console.log(false)
             }
             n++
-        }return true
+        }return console.log(true) 
 }
 
-console.log(isSymmetrical(a))
+isSymmetrical(a)
+isSymmetrical(b)
+isSymmetrical(c)
