9d3a22fdcd59fcbac2909de1502847297002337c
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-09-01 12:27:22 +0000

    Update Convert Key, Values in an Object to Array.js

--- Convert Key, Values in an Object to Array.js
+++ Convert Key, Values in an Object to Array.js
@@ -4,8 +4,15 @@ let array = {
   C: 3
 }
 
+let array2 = {
+  AD: 12,
+  AB: 22,
+  AC: 32
+}
+//fonction qui transforme les information d'un objet en tableau 
 function objectToArray(obj) {
 	let b = Object.entries(obj)
     return console.log(b)
 }
 objectToArray(array)  
+objectToArray(array2)  
